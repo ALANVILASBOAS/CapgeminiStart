8286a884293ec7f4e31cca4411529f9567598594
ALLANVILASBOAS <alan07vb@gmail.com>
2022-03-26 01:30:34 -0300

    add router

---
+++ 07.Angular/05.router/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
+
+const routes: Routes = [
+  {path: 'inicio', component:InicioComponent},
+  {path: 'sobre', component: SobreComponent},
+  {path: '', redirectTo:'inicio', pathMatch:'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  
+
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
